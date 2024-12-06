35dc9cf4555809aa8bd7756582404f502f5d5a2c
SantiagoRS26 <santiagors2611@gmail.com>
2024-12-05 23:07:47 -0500

    update

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -49,11 +49,14 @@ export default async function MoviePage({ params }: MoviePageProps) {
 			<div className="mt-5">
 				<MovieRecommendations recommendations={recommendations.results} />
 			</div>
-			<Link href="/" aria-label="Ir al Home">
-				<button
-					className="fixed bottom-5 right-5 bg-yellow-500 text-black p-3 rounded-full shadow-md hover:bg-yellow-600 transition flex items-center justify-center z-10"
-				>
-					<FaHome size={20} className="mr-2" />
+			<Link
+				href="/"
+				aria-label="Ir al Home">
+				<button className="fixed bottom-5 right-5 bg-yellow-500 text-black p-3 rounded-full shadow-md hover:bg-yellow-600 transition flex items-center justify-center z-10">
+					<FaHome
+						size={20}
+						className="mr-2"
+					/>
 					<span>Home</span>
 				</button>
 			</Link>
